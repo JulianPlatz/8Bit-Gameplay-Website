8fa582f29fda50166f64bfd2e8da6538e4e18d96
JulianPlatz <julian.platz@icloud.com>
2024-01-25 23:14:29 +0100

    better readable

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const marioButton = document.getElementById('marioButton');
-const sonicButton = document.getElementById('sonicButton');
+const marioButton       = document.getElementById('marioButton');
+const sonicButton       = document.getElementById('sonicButton');
 
-const head = document.getElementById('head');
-const selectGameText = document.getElementById('selectGameText');
+const head              = document.getElementById('head');
+const selectGameText    = document.getElementById('selectGameText');
 
-const marioVideo = document.getElementById('marioVideo');
-const sonicVideo = document.getElementById('sonicVideo');
+const marioVideo        = document.getElementById('marioVideo');
+const sonicVideo        = document.getElementById('sonicVideo');
 
 function hideButton(button) {
-    head.style.opacity = "0";
-    button.style.opacity = "0";
-    button.style.transition = ".5s";
-    button.style.cursor = "default";
-    selectGameText.style.opacity = "0";
+    head.style.opacity              = "0";
+    button.style.opacity            = "0";
+    button.style.transition         = ".5s";
+    button.style.cursor             = "default";
+    selectGameText.style.opacity    = "0";
     button.classList.add('noPseudoAfter');
     button.classList.add('noPseudoBefore');
 
@@ -23,13 +23,13 @@ function hideButton(button) {
 
 function playVideo(button, video) {
     if (button.style.display !== "none") {
-        video.style.opacity = "0";
-        video.style.display = "block";
-        button.style.display = "none";
+        video.style.opacity     = "0";
+        video.style.display     = "block";
+        button.style.display    = "none";
 
         setTimeout(function() {
-            video.style.opacity = "1";
-            video.style.transition = ".5s";
+            video.style.opacity     = "1";
+            video.style.transition  = ".5s";
         }, 0);
         video.play();
     }
